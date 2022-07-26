6aeff54a19f05a9bb25f1cea0a3ffe68336b2b4d
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-07-26 13:56:48 +0900

    JSDOM is used in test (#25)

--- test/test.ts
+++ test/test.ts
@@ -1,8 +1,21 @@
 import {describe, it } from "mocha";
 import { Tag } from "../src/tag";
 import { strict as assert } from "assert";
+import { JSDOM } from "jsdom";
+import * as path from "path";
+
+const htmlText = `
+<!DOCTYPE html>
+<html>
+  <body></body>
+</html>
+`;
 
 describe("Tag class", () => {
+  beforeEach(async () => {
+    global.document = new JSDOM(htmlText).window.document;
+  });
+
   it("property name", () => {
     const tagName = "a";
     assert.equal(
@@ -34,6 +47,32 @@ describe("Tag class", () => {
         new Tag("a", { styles }).stylesTemplate
       ),
       JSON.stringify(styles)
+    );
+  });
+
+  it("Custom class", () => {
+    const valueChanged = "value-changed";
+    class CustomAnchor extends Tag {
+      constructor(){
+        super("a");
+      }
+      public create(){
+        const element = super.create();
+        element.addEventListener("click", (event) => {
+          // @ts-ignore
+          event.target.innerHTML = valueChanged;
+        });
+        return element;
+      }
+    }
+
+    document.body.appendChild(new CustomAnchor().create());
+    Array.from(document.querySelectorAll("a"))
+      .forEach(element => element.click());
+
+    assert.equal(
+      (document.querySelector("a") as HTMLElement).innerHTML,
+      valueChanged
     );
   });
 
